709022f19f025223f586cb01bbfe5730cb3a225b
marekryb96 <marekrybicki96@gmail.com>
2017-12-06 19:07:15 +0100

    correct  sql
    correct  sql

---
+++ Database/procedury.sql
@@ -0,0 +1,10 @@
+USE League;
+
+
+EXEC InsertKlient 5, "Robert", "Gadocha", 250, 88, "Poland"
+
+EXEC DeleteKlient 25, "Robert", "Gadocha", 187, 88, "Poland"
+
+EXEC UpdateKlient 25, "Robert", "Gzandocha", 187, 98, "Poland"
+
+select * from Players
